a526151da685a0eac4c83c1c1fa9ac81ac120c91
adii1203 <aditya32ft@gmail.com>
2023-08-06 22:46:35 +0530

    origin

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://celebrated-fenglisu-f91ead.netlify.app",
     credentials: true,
     // methods: "GET POST",
   })
