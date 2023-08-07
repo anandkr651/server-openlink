5034b7770c0af30dfcc6536bac4dfb6d744da39d
adii1203 <aditya32ft@gmail.com>
2023-08-07 13:49:51 +0530

    fix samesite none

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -44,7 +44,7 @@ export const Login = async (req, res) => {
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: true,
-        SameSite: "none",
+        SameSite: "None",
       })
       .status(200)
       .json(
